66cf5ef60f78cde66d4009b9249c24198d0c89ac
prathiksh13 <prathiksh15@gmail.com>
2026-04-23 21:50:49 +0530

    Update socket.js

--- frontend-react/src/lib/socket.js
+++ frontend-react/src/lib/socket.js
@@ -1,8 +1,7 @@
-// socket.js
+import { io } from "socket.io-client"
 
-import { io } from "socket.io-client";
-import { socketUrl } from "./api";
+const SOCKET_URL = import.meta.env.VITE_NODE_API_URL
 
-export const socket = io(socketUrl(), {
+export const socket = io(SOCKET_URL, {
   transports: ["websocket"],
-});
+})
